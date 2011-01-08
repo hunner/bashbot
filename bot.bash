babd90296cc532eb6b2f8d6c478aa8d6bc938a96
Hunter Haugen <h.haugen@gmail.com>
2011-01-07 20:42:18 -0800

    Initial commit

---
+++ bot.bash
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+line=""
+started=""
+rm botfile
+mkfifo botfile
+tail -f botfile | nc irc.cat.pdx.edu 6667 | while true ; do
+    if [ -z $started ] ; then
+        echo "USER bdbot 0 bdbot :I iz a bot" > botfile
+        echo "NICK bdbot" >> botfile
+        echo "JOIN #notzombies" >> botfile
+        started="yes"
+    fi
+    read irc
+    case `echo $irc | cut -d " " -f 1` in
+        "PING") echo "PONG :`hostname`" >> botfile ;;
+    esac
+    #echo $irc
+    chan=`echo $irc | cut -d ' ' -f 3`
+    barf=`echo $irc | cut -d ' ' -f 1-3`
+    cmd=`echo ${irc##$barf :}|cut -d ' ' -f 1|tr -d "\r\n"`
+    args=`echo ${irc##$barf :$cmd}|tr -d "\r\n"`
+    nick="${irc%%!*}";nick="${nick#:}"
+    if [ "`echo $cmd | cut -c1`" == "!" ] ; then
+        echo "Got command $cmd from channel $chan with arguments $args"
+    fi
+    case $cmd in
+        "!add") line="$args $line" ;;
+        "!list") echo "PRIVMSG $chan :$line" >> botfile ;;
+    esac
+done
+
